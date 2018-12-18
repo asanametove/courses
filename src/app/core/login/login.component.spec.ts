07c5de59f88bcb7de9438301b90cb3979427d041
Asan Ametov <Asan_Ametov@epam.com>
2018-12-18 14:27:46 +0200

    Test approach of Login component changed

--- src/app/core/login/login.component.spec.ts
+++ src/app/core/login/login.component.spec.ts
@@ -1,35 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new LoginComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should log in', () => {
-    component.logIn();
-    expect(component.isLoggedIn).toBe(true);
+  describe('#logIn', () => {
+    it('should log in', () => {
+      component.logIn();
+      expect(component.isLoggedIn).toBe(true);
+    });
   });
 
-  it('should log out', () => {
-    component.logOut();
-    expect(component.isLoggedIn).toBe(false);
+  describe('#logOut', () => {
+    it('should log out', () => {
+      component.logOut();
+      expect(component.isLoggedIn).toBe(false);
+    });
   });
 });
