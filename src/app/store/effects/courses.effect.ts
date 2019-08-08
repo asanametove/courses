cff625265997750a45acbde0039d7970cc7afa6c
Asan Ametov <asan_ametov@epam.com>
2019-08-08 14:32:02 +0300

    [NGRX] Effects fixed

--- src/app/store/effects/courses.effect.ts
+++ src/app/store/effects/courses.effect.ts
@@ -2,18 +2,14 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { map, mergeMap, catchError, tap } from 'rxjs/operators';
-import { ActionTypes as CoursesActionTypes, SaveCourses, CoursesActions, LoadCourses } from '@store/actions/courses-page.actions';
+import { ActionTypes as CoursesActionTypes, SaveCourses } from '@store/actions/courses-page.actions';
 import {
   ActionTypes as CourseDetailsActionTypes,
-  LoadCourseDetails,
-  CourseDetailsActions,
   SaveCourseDetails,
-  UpdateCourseDetails,
   UpdateCourseDetailsSuccess,
 } from '@store/actions/course-details-page.actions';
 import { CoursesService } from '@core/courses/courses.service';
 import { Course } from '@shared/course';
-import { Action } from '@ngrx/store';
 import { NavigationService } from '@core/navigation/navigation.service';
 import { RouteName } from '@shared/route-name';
 
@@ -28,7 +24,7 @@ export class CoursesEffects {
   @Effect()
   loadCourses$ = this.actions$.pipe(
     ofType(CoursesActionTypes.Load),
-    mergeMap<LoadCourses, CoursesActions>(({ payload }) => this.coursesService.getCourses(payload).pipe(
+    mergeMap(({ payload }) => this.coursesService.getCourses(payload).pipe(
       map(courses => new SaveCourses(courses)),
       catchError(() => of(new SaveCourses([]))),
     )),
@@ -37,7 +33,7 @@ export class CoursesEffects {
   @Effect()
   loadCourseDetails$ = this.actions$.pipe(
     ofType(CourseDetailsActionTypes.Load),
-    mergeMap<LoadCourseDetails, CourseDetailsActions>(({ payload }) => this.coursesService.getCourseById(payload).pipe(
+    mergeMap(({ payload }) => this.coursesService.getCourseById(payload).pipe(
       map(course => new SaveCourseDetails(course)),
       catchError(() => of(new SaveCourseDetails(new Course()))),
     )),
@@ -46,7 +42,7 @@ export class CoursesEffects {
   @Effect()
   updateCourseDetails$ = this.actions$.pipe(
     ofType(CourseDetailsActionTypes.Update),
-    mergeMap<UpdateCourseDetails, Action>(({payload}) => this.coursesService.createCourse(payload).pipe(
+    mergeMap(({payload}) => this.coursesService.createCourse(payload).pipe(
       map(() => new UpdateCourseDetailsSuccess()),
       tap(() => this.navigationService.navigateByUrl(RouteName.Courses)),
     )),
